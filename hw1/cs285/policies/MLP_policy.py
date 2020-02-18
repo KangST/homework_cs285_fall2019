02cc6b3592913db53fe9d71c0e8c3c0db1a86250
KangST <cdef3456@naver.com>
2020-02-18 18:55:08 +0900

    hw1

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -50,6 +50,7 @@ class MLPPolicy(BasePolicy):
     ##################################
 
     def define_placeholders(self):
+        self.observations_pl = tf.placeholder(dtype=tf.float32, shape=self.ob_dim)
         raise NotImplementedError
 
     def define_forward_pass(self):
@@ -87,7 +88,7 @@ class MLPPolicy(BasePolicy):
         # HINT1: you will need to call self.sess.run
         # HINT2: the tensor we're interested in evaluating is self.sample_ac
         # HINT3: in order to run self.sample_ac, it will need observation fed into the feed_dict
-        return TODO
+        return self.sess.run(self.sample_ac, feed_dict={self.observations_pl: observation})
 
     # update/train this policy
     def update(self, observations, actions):
@@ -118,10 +119,10 @@ class MLPPolicySL(MLPPolicy):
         true_actions = self.acs_labels_na
         predicted_actions = self.sample_ac
 
-        # TODO define the loss that will be used to train this policy
+        # define the loss that will be used to train this policy
         # HINT1: remember that we are doing supervised learning
         # HINT2: use tf.losses.mean_squared_error
-        self.loss = TODO
+        self.loss = tf.losses.mean_squared_error(true_actions, predicted_actions)
         self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
     def update(self, observations, actions):
